8bb5a581c0ddc2c4dfb97300142c8439734fb100
unknown <riyad298@gmail.com>
2020-07-03 00:21:53 +0600

    anonymouns inner class

--- src/greet/Greeter.java
+++ src/greet/Greeter.java
@@ -11,10 +11,32 @@ public class Greeter {
 		// greeter.Greet();
 		
 		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
-		greeter.Greet(helloWorldGreeting);
+//		greeter.Greet(helloWorldGreeting);
 		
-		
+		MyLamda myLamdaFunction = () -> System.out.println("Hellow world");
 	
+		Greeting functionalInterface = () -> System.out.println("Hellow world");
+		
+		
+		//anonymous inner class
+		Greeting innerClassInterface = new Greeting() {
+			public void perform() {
+				System.out.println("Hellow world");
+			}
+		};
+		
+		greeter.Greet(functionalInterface);
+		greeter.Greet(innerClassInterface);
+		
+		
+		
 	}
 	
 }
+
+
+interface MyLamda {
+	public void foo();
+}
+
+
